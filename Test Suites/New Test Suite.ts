a4ffda10629d34116cc07451f51edfeda0fc565b
quan.trinh <quan.trinh@GF0X2D3>
2022-03-08 16:58:03 +0700

    a

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7de1034c-5850-42d7-acdb-a9261a25cd25</testSuiteGuid>
+   <testCaseLink>
+      <guid>82a4fd72-6369-4a7d-9b6e-6767ffb10823</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Navigate to google</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>996e529a-e3ab-4dc7-9197-268e84a7aa2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
